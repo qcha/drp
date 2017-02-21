f3bc12484b4a6d6bbadafc974c4f7dec9e2e5908
AKuchuk <akuchuk@at-consulting.ru>
2017-02-21 11:24:00 +0300

    Char instead Character

--- src/main/java/com/github/aarexer/qcha/drp/model/DsvFileResource.java
+++ src/main/java/com/github/aarexer/qcha/drp/model/DsvFileResource.java
@@ -12,23 +12,23 @@ import java.nio.charset.StandardCharsets;
 @Getter
 public class DsvFileResource {
     private static final Logger logger = LogManager.getLogger();
-    private static final Character DEFAULT_QUOTES = '"';
-    private static final Character DEFAULT_DELIMITER = ';';
+    private static final char DEFAULT_QUOTES = '"';
+    private static final char DEFAULT_DELIMITER = ';';
     private static final String DEFAULT_LINE_SEPARATOR = System.lineSeparator();
     private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
     @NonNull
     private final URI uri;
     @NonNull
-    private final Character delimiter;
+    private final char delimiter;
+    private final char quotes;
     private final Charset encoding;
     private final String lineSeparator;
-    private final Character quotes;
     //todo it should not be strings
     private final String archiveType;
     private final String compressType;
 
-    public DsvFileResource(URI uri, Character delimiter, Charset encoding, String lineSeparator, Character quotes, String archiveType, String compressType) {
+    public DsvFileResource(URI uri, char delimiter, Charset encoding, String lineSeparator, char quotes, String archiveType, String compressType) {
         this.uri = uri;
         this.delimiter = delimiter;
         this.encoding = encoding;
@@ -42,7 +42,7 @@ public class DsvFileResource {
         this(uri, DEFAULT_DELIMITER, DEFAULT_CHARSET, DEFAULT_LINE_SEPARATOR, DEFAULT_QUOTES, null, null);
     }
 
-    public DsvFileResource(URI uri, Character delimiter, String archiveType, String compressType) {
+    public DsvFileResource(URI uri, char delimiter, String archiveType, String compressType) {
         this(uri, delimiter, StandardCharsets.UTF_8, System.lineSeparator(), DEFAULT_QUOTES, archiveType, compressType);
     }
 }
