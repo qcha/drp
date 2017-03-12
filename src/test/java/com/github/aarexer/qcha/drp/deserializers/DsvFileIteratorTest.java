e07560898df31aea74e578246a475a18dded9392
aarexer <aarexer@gmail.com>
2017-03-12 14:50:44 +0300

    Init testing

--- src/test/java/com/github/aarexer/qcha/drp/deserializers/DsvFileIteratorTest.java
+++ src/test/java/com/github/aarexer/qcha/drp/deserializers/DsvFileIteratorTest.java
@@ -1,12 +1,35 @@
 package com.github.aarexer.qcha.drp.deserializers;
 
-import org.junit.Before;
+import com.github.aarexer.qcha.drp.model.DsvPreference;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Objects;
 
 public class DsvFileIteratorTest {
     private DsvFileIterator dsvFileIterator;
 
-    @Before
-    public void setUp() throws Exception {
-//        dsvFileIterator = new DsvFileIterator()
+    @Test
+    public void parseFirstLineOfThePlainFile() throws Exception {
+        dsvFileIterator = new DsvFileIterator(getClass().getResourceAsStream("FileTest.csv"), new DsvPreference());
+        Assert.assertEquals(dsvFileIterator.hasNext(), true);
+        final List<String> lst = dsvFileIterator.next();
+        Assert.assertEquals(Objects.isNull(lst), false);
+        Assert.assertEquals(lst.size(), 2);
+        Assert.assertEquals(lst.get(0), "value1");
+        Assert.assertEquals(lst.get(1), "value2");
+    }
+
+    @Test
+    public void parseSecondLineOfThePlainFile() throws Exception {
+        dsvFileIterator = new DsvFileIterator(getClass().getResourceAsStream("FileTest.csv"), new DsvPreference());
+        Assert.assertEquals(dsvFileIterator.hasNext(), true);
+        // skip first line
+        dsvFileIterator.next();
+        final List<String> lst = dsvFileIterator.next();
+        Assert.assertEquals(Objects.isNull(lst), false);
+        Assert.assertEquals(lst.size(), 3);
+        //todo
     }
 }
